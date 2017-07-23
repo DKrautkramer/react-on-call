519421a5a112d2f332e3c1f29e230246cc2615a7
Derek Krautkramer <derek.krautkramer@gmail.com>
2017-07-23 11:04:27 -0500

    Added base routing for components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import List from './components/list/List';
 import Users from './components/users/Users';
 import Departments from './components/departments/Departments';
 import './App.css';
 
 class App extends Component {
+
     render() {
         return (
-            <div className="container">
+            <div className='container'>
                 <h1>On Call Database</h1>
 
-                <List></List>
-                <hr/>
-                <Users></Users>
-                <hr/>
-                <Departments></Departments>
+                <BrowserRouter>
+                    <div>
+                        <Route path='/list' component={ List } />
+                        <Route path='/users' component={ Users } />
+                        <Route path='/departments' component={ Departments } />
+                    </div>
+                </BrowserRouter>
 
             </div>
         );
